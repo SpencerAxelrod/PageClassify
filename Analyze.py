5a83ff1ec770dcabacbb10e2e509f2f429328163
Spencer Axelrod <spencer.axelrod@gmail.com>
2016-04-14 16:40:43 -0700

    fetches HTML with spoofing for certain websites

--- Analyze.py
+++ Analyze.py
@@ -1,15 +1,28 @@
 import sys
 import urllib.request	#python3
+import requests
 from bs4 import BeautifulSoup
 
+def fetch_html(page_url):
+	"""Returns the HTML of the page at page_url"""
+	html = ""
+	#spoof header for sites like Amazon that make it more dfficult to scrape 
+	req = urllib.request.Request(page_url, headers={ 'User-Agent': 'Mozilla/5.0' })
+	html = urllib.request.urlopen(req).read()
+	return html
 
-def AnalyzePage(page_url):
 
-	#Fetch HTML
-	response = urllib.request.urlopen(page_url)
-	html = response.read()
-	soup = BeautifulSoup(html)
-	print(soup.prettify())
+def analyze_page(page_url):
+	"""Analyzes the content at page_url and returns a that best describe the contents of that page"""
+	
+	html = fetch_html(page_url)
+	soup = BeautifulSoup(html, "html.parser")
+	#print(soup.prettify())
+	#titleTag = soup.html.head.title
+	#print(titleTag.string)
+	print(soup.title.string)
+	
+
 
 def main():
     
@@ -17,7 +30,7 @@ def main():
     	print("Please enter a single argument")
     	return
 
-    AnalyzePage(sys.argv[1])
+    analyze_page(sys.argv[1])
 
 
 if __name__ == "__main__":
