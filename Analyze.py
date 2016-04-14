6306b3904edf73a2ad269402603bae58744eecf9
Spencer Axelrod <spencer.axelrod@gmail.com>
2016-04-14 02:06:33 -0700

    Fetch HTML from page

--- Analyze.py
+++ Analyze.py
@@ -1,5 +1,24 @@
+import sys
+import urllib.request	#python3
+from bs4 import BeautifulSoup
+
+
+def AnalyzePage(page_url):
+
+	#Fetch HTML
+	response = urllib.request.urlopen(page_url)
+	html = response.read()
+	soup = BeautifulSoup(html)
+	print(soup.prettify())
+
 def main():
-    print("getting Started")
+    
+    if len(sys.argv) != 2:
+    	print("Please enter a single argument")
+    	return
+
+    AnalyzePage(sys.argv[1])
+
 
 if __name__ == "__main__":
     main()
