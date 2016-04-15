083d3f95fed51b875abc2f79c758f71373417dc7
Spencer Axelrod <spencer.axelrod@gmail.com>
2016-04-14 21:23:10 -0700

    retrieve word counts

--- Analyze.py
+++ Analyze.py
@@ -5,10 +5,16 @@ from bs4 import BeautifulSoup
 
 def fetch_html(page_url):
 	"""Returns the HTML of the page at page_url"""
+
 	html = ""
 	#spoof header for sites like Amazon that make it more dfficult to scrape 
-	req = urllib.request.Request(page_url, headers={ 'User-Agent': 'Mozilla/5.0' })
-	html = urllib.request.urlopen(req).read()
+	try:
+		req = urllib.request.Request(page_url, headers={ 'User-Agent': 'Mozilla/5.0' })
+		html = urllib.request.urlopen(req).read()
+	except urllib.error.URLError as e:
+		print("page request failed: " + repr(e))
+		return
+
 	return html
 
 
@@ -16,12 +22,33 @@ def analyze_page(page_url):
 	"""Analyzes the content at page_url and returns a that best describe the contents of that page"""
 	
 	html = fetch_html(page_url)
+	if not html:
+		return
+
 	soup = BeautifulSoup(html, "html.parser")
 	#print(soup.prettify())
 	#titleTag = soup.html.head.title
 	#print(titleTag.string)
-	print(soup.title.string)
-	
+	#print(soup.find_all())
+
+	word_counts = {}
+	ignore_tags = ["script", "div", "img", "meta", "span"]
+	for tag in soup.find_all():
+		#if tag.name == "span":
+			#print()
+			#print(tag.name + " \" " + " ".join(tag.text.strip().split()) +" \" ")
+
+		words = tag.text.split()
+		for word in words:
+			word_lower = word.lower()
+			if word_lower in word_counts:
+				word_counts[word_lower] = word_counts[word_lower] + 1
+			else:
+				word_counts[word_lower] = 1
+		#print(words)
+	print(sorted(word_counts.items(), key=lambda x:x[1]))
+
+
 
 
 def main():
